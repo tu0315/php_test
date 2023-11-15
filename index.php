96982ad7b3ee8e8b76cca0e7be3b573806363dbb
uu0315 <takurou.usuki@gmail.com>
2023-11-15 12:57:25 +0900

    L14まで

--- index.php
+++ index.php
@@ -20,8 +20,80 @@
     // var_dump($test_3);
 
     // 定数constantは全て大文字
-    const MAX = 100;
-    echo MAX;
+    // const MAX = 100;
+    // echo MAX;
 
+    // // 1行の配列
+    // $array_1 = ['aaa', 2, 3];
+
+    // // 2行の配列
+    // $array_2 = [
+    //     ['赤', '青', '黄'],
+    //     ['緑', '紫', '黒']
+    // ];
+
+    // echo '<pre>';
+    // var_dump($array_1);
+    // echo '</pre>';
+
+    // echo '<pre>';
+    // var_dump($array_2);
+    // echo '</pre>';
+
+    // echo $array_2[1][1];
+
+    // // 連想配列（文字列がキーになっている配列）
+    // $array_member = [
+    //     'name' => '本田',
+    //     'height' => 170,
+    //     'hobby' => 'サッカー'
+    // ];
+
+    // echo '<pre>';
+    // var_dump($array_member);
+    // echo '</pre>';
+
+    // echo $array_member['hobby'];
+
+    // $array_member_2 = [
+    //     '本田' => [
+    //         'height' => 170,
+    //         'hobby' => 'サッカー'
+    //     ],
+    //     '香川' => [
+    //         'height' => 165,
+    //         'hobby' => 'ゲーム'
+    //     ],
+    // ];
+
+    // echo '<pre>';
+    // var_dump($array_member_2['本田']);
+    // var_dump($array_member_2['香川']['hobby']);
+    // echo '</pre>';
+
+    // $array_member_3 = [
+    //     '1組' => [
+    //         '本田' => [
+    //             'height' => 170,
+    //             'hobby' => 'サッカー'
+    //         ],
+    //         '長友' => [
+    //             'height' => 195,
+    //             'hobby' => 'サッカー'
+    //         ],
+    //     ],
+    //     '2組' => [
+    //         '香川' => [
+    //             'height' => 165,
+    //             'hobby' => 'ゲーム'
+    //         ],
+    //     ],
+    // ];
+
+    // echo '<pre>';
+    // var_dump($array_member_3['1組']);
+    // var_dump($array_member_3['2組']['香川']);
+    // var_dump($array_member_3['2組']['香川']['height']);
+    // echo '</pre>';
     ?>
 </body>
