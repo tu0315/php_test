1bc4aee51ce6731fbdc239831dc1544d046680a7
uu0315 <takurou.usuki@gmail.com>
2023-11-15 10:22:44 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>PHPテスト環境</head>
+
+<body>
+    <h1>こちらはPHPテスト環境です。</h1>
+
+    <?php
+
+    // echo ('PHPです!');
+    // echo ('<br>');
+    // echo ('PHPです!!');
+    // phpinfo();
+
+    // $test_1 = 123;
+    // $test_2 = 456;
+    // $test_3 = $test_1 . $test_2;
+
+    // 変数デバックはvar_dump()で
+    // var_dump($test_3);
+
+    // 定数constantは全て大文字
+    const MAX = 100;
+    echo MAX;
+
+    ?>
+</body>
